41bac0d2b5f8535bd166ce339e6b75684a19d4e8
nisha <nisharma639@gmail.com>
2023-10-17 14:59:30 +0530

    initial

---
+++ javaScript.js
@@ -0,0 +1,22 @@
+gsap.to(".imagecontainter",{
+    ease: Expo.easeInOut,
+    width:"100%",
+    stagger : 2,
+    
+})
+
+gsap.to(".text h1",{
+    ease: Expo.easeInOut,
+    width:"100%",
+    stagger : 2,
+    top : 0
+})
+
+gsap.to(".text h1",{
+    delay: 2,
+    ease: Expo.easeInOut,
+    width:"100%",
+    stagger : 2,
+    top : "-100%"
+})
+
